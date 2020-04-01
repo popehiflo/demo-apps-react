1d076bd731cf7614bc6c8375e6159e953c60812d
popehiflo <poolpetterhijuel@hotmail.com>
2020-04-01 12:26:42 -0500

    modificando componente (destructuring) para las props

--- personal-trainer-app/src/components/Card.jsx
+++ personal-trainer-app/src/components/Card.jsx
@@ -1,22 +1,26 @@
 import React, { Component } from "react"
-import stretchesImg from "../images/stretches2.png"
 import circlesbackImg from "../images/circlesback.png"
 import "./styles/Card.css"
 
 class Card extends Component {
+
+    
     //background: url('../../images/circlesback.png') no-repeat, linear-gradient(to right, #A74CF2, #617BFB);
     render(){
+
+        const { title, description, img, leftColor, rightColor } = this.props
+
         return (
             <div className="card mx-auto Fitness-Card" style={
-                {backgroundImage: `url(${circlesbackImg}), linear-gradient(to right,${this.props.leftColor} , ${this.props.rightColor})`}}>
+                {backgroundImage: `url(${circlesbackImg}), linear-gradient(to right,${leftColor} , ${rightColor})`}}>
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={this.props.img} className="float-right" alt="personal" />
+                            <img src={img} className="float-right" alt="personal" />
                         </div>
                         <div className="col-6 Fitness-Card-Info">
-                            <h1>{this.props.title}</h1>
-                            <p>{this.props.description}</p>
+                            <h1>{title}</h1>
+                            <p>{description}</p>
                         </div>
                     </div>
                 </div>
