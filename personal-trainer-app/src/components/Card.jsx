61c578e1279f4b999bcf7ca2f324378953474079
popehiflo <poolpetterhijuel@hotmail.com>
2020-04-01 11:40:23 -0500

    cambiando por estilos en linea, moficando card para pasar props

--- personal-trainer-app/src/components/Card.jsx
+++ personal-trainer-app/src/components/Card.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react"
 import stretchesImg from "../images/stretches2.png"
+import circlesbackImg from "../images/circlesback.png"
 import "./styles/Card.css"
 
 class Card extends Component {
+    //background: url('../../images/circlesback.png') no-repeat, linear-gradient(to right, #A74CF2, #617BFB);
     render(){
         return (
-            <div className="card mx-auto Fitness-Card">
+            <div className="card mx-auto Fitness-Card" style={
+                {backgroundImage: `url(${circlesbackImg}), linear-gradient(to right, #A74CF2, #617BFB)`}}>
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={stretchesImg} className="float-right" />
+                            <img src={stretchesImg} className="float-right" alt="personal" />
                         </div>
                         <div className="col-6 Fitness-Card-Info">
                             <h1>Technique Guides</h1>
