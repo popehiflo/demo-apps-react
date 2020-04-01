f4624145124144efcec160c23f305392de310977
popehiflo <poolpetterhijuel@hotmail.com>
2020-03-31 19:20:02 -0500

    Despues de probar con JS ahora pruebo como lo hace React

--- personal-trainer-app/src/index.js
+++ personal-trainer-app/src/index.js
@@ -1,8 +1,16 @@
 
-const elemento = document.createElement('h1')
+// const elemento = document.createElement('h1')
 
-elemento.innerHTML = "Hola React!"
+// elemento.innerHTML = "Hola React!"
 
-const conteneder = document.getElementById('root')
+// const conteneder = document.getElementById('root')
 
-conteneder.appendChild(elemento)
+// conteneder.appendChild(elemento)
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+const elemento = <h1>Hi React!</h1>
+const container = document.getElementById('root')
+
+ReactDOM.render(elemento, container)
