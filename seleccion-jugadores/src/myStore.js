9fecdb3335bb5e6b2de88a6fcd2292e1c9f4ff99
popehiflo <poolpetterhijuel@hotmail.com>
2020-04-04 19:37:11 -0500

    agregando foto por defecto para jugadores y tambien agregando fotos reales de estos. Imagen agregada al folder principal para ver la app

--- seleccion-jugadores/src/myStore.js
+++ seleccion-jugadores/src/myStore.js
@@ -1,22 +1,83 @@
-import {createStore} from "redux"
+import {createStore} from "redux";
+import imgJugador from "./fotoJugador.jpg";
 
 
 const initialState = {
     jugadores: [
         {
             id: 1,
-            nombre: "Pool Petter Hijuela",
+            nombre: "Pool Petter",
             foto: "https://avatars0.githubusercontent.com/u/14990458?s=400&u=a8c491d0926f040191dbe123ec2da96c292e6b7e&v=4"
         },
         {
             id: 2,
-            nombre: "Java EE",
-            foto: "https://avatars0.githubusercontent.com/u/26341556?s=400&u=dac0dde41a0ff26b383b651d1510712e107fbbd1&v=4"
+            nombre: "Claudio Pizarro",
+            foto: "https://st-listas.20minutos.es/images/2013-07/364838/4092747_640px.jpg?1373560046"
         },
         {
             id: 3,
-            nombre: "Thiago Pro",
-            foto: "https://avatars2.githubusercontent.com/u/721009?s=400&u=104044d16ec134bdb3e0b661312aa49b2a71832a&v=4"
+            nombre: "Paolo Guerrero",
+            foto: "https://st-listas.20minutos.es/images/2013-07/364838/4092750_640px.jpg?1373560046"
+        },
+        {
+            id: 4,
+            nombre: "Teofilo Cubillas",
+            foto: "https://st-listas.20minutos.es/images/2013-07/364838/4092736_640px.jpg?1373560046"
+        },
+        {
+            id: 5,
+            nombre: "Hugo Sotil",
+            foto: "https://st-listas.20minutos.es/images/2013-07/364838/4092739_640px.jpg?1373560046"
+        },
+        {
+            id: 6,
+            nombre: "Cesar Cueto",
+            foto: "https://st-listas.20minutos.es/images/2013-07/364838/4092742_640px.jpg?1373560046"
+        },
+        {
+            id: 7,
+            nombre: "Jefferson Farfan",
+            foto: "https://st-listas.20minutos.es/images/2013-07/364838/4092749_640px.jpg?1373560046"
+        },
+        {
+            id: 8,
+            nombre: "Hector Chumpitaz",
+            foto: "https://st-listas.20minutos.es/images/2013-07/364838/4092738_640px.jpg?1373560046"
+        },
+        {
+            id: 9,
+            nombre: "Teodoro Fernandez",
+            foto: "https://st-listas.20minutos.es/images/2013-07/364838/4092737_640px.jpg?1373560046"
+        },
+        {
+            id: 10,
+            nombre: "Julio Cersar Uribe",
+            foto: "https://st-listas.20minutos.es/images/2013-07/364838/4092751_640px.jpg?1373560046"
+        },
+        {
+            id: 11,
+            nombre: "Juan Carlos oblitas",
+            foto: "https://st-listas.20minutos.es/images/2013-07/364838/4092741_640px.jpg?1373560046"
+        },
+        {
+            id: 12,
+            nombre: "Nolberto Solano",
+            foto: imgJugador
+        },
+        {
+            id: 13,
+            nombre: "Roberto Palacios",
+            foto: imgJugador
+        },
+        {
+            id: 14,
+            nombre: "Juan Manuel Vargas",
+            foto: imgJugador
+        },
+        {
+            id: 15,
+            nombre: "Roberto Challe",
+            foto: imgJugador
         }
     ],
     titulares: [],
